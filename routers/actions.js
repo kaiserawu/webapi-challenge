8a685f82dc5e3bac4743849cc695242f34e1503a
kaiserawu <kaiser.a.wu@gmail.com>
2019-05-18 11:52:52 -0700

    Implemented POST endpoint for actions

--- routers/actions.js
+++ routers/actions.js
@@ -21,7 +21,22 @@ router.get('/:id', async (req, res) => {
     res.json(data);
   } catch(err) {
     if (err instanceof TypeError) {
-      res.status(404).json({ message: 'Error that ID was not found.' });
+      res.status(404).json({ message: 'Error, that ID was not found.' });
+    } else {
+      res.status(500).json(err);
+    }
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const postData = req.body;
+    const returnedData = await db.insert(postData);
+
+    res.json(returnedData);
+  } catch(err) {
+    if (err.errno === 19) {
+      res.status(404).json({ message: 'Error, that project ID was not found.' });
     } else {
       res.status(500).json(err);
     }
